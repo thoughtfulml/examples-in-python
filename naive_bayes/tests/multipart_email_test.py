5d715e1c1182b25d021421426e0d9188853b8b4b
Matthew Kirk <meteor.kirk@gmail.com>
2015-07-26 11:43:52 -0700

    Multipart email works

--- naive_bayes/tests/multipart_email_test.py
+++ naive_bayes/tests/multipart_email_test.py
@@ -6,8 +6,10 @@ from naive_bayes.email_object import EmailObject
 class TestMultipartEmailObject(unittest.TestCase):
   def setUp(self):
     self.multipart_file = './tests/fixtures/multipart.eml'
-    self.multipart = io.open(self.multipart_file, 'r').read()
-    self.multipart_email = EmailObject(self.multipart_file)
+    self.multipart = io.open(self.multipart_file, 'r')
+    self.text = self.multipart.read()
+    self.multipart.seek(0)
+    self.multipart_email = EmailObject(self.multipart)
 
   def test_parse_concatenated_body_of_text(self):
     internal_mail = self.multipart_email.mail
@@ -16,12 +18,11 @@ class TestMultipartEmailObject(unittest.TestCase):
     body = ''
     for part in internal_mail.walk():
       if re.match("text/plain", part.get_content_type()):
-        body += str(part)
+        body += part.get_payload(decode=True)
       elif re.match("text/html", part.get_content_type()):
-        body += str(part)
-
-    self.assertEqual(self.multipart_email.body, body)
+        body += part.get_payload(decode=True) 
+    self.assertEqual(self.multipart_email.body(), body)
 
   def test_stores_subject(self):
-    subject = re.match("Subject: (.*)$", self.multipart).group(1)
-    self.assertEqual(self.multipart_email.subject, subject)
+    subject = re.search("Subject: (.*)", self.text).group(1)
+    self.assertEqual(self.multipart_email.subject(), subject)
