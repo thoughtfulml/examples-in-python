274b774a6ae8a264449978ce9d2be22ecd1854cc
Alexey Porotnikov <alexey.porotnikov@gmail.com>
2017-06-26 23:23:31 +0300

    code cleaning

--- decision_trees/classifier.py
+++ decision_trees/classifier.py
@@ -1,26 +1,52 @@
+"""
+Chapter 5. Decision trees
+"""
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
 from numpy.random import permutation
 from numpy import array_split, concatenate
-from sklearn.metrics import roc_curve, auc, mean_squared_error
+from sklearn.metrics import mean_squared_error
 import pandas as pd
 import numpy as np
 
+
 class MushroomProblem:
+  """
+  Mushrooms classification problem
+  """
+
   def __init__(self, data_file):
-    self.dataFrame = pd.read_csv(data_file)
-    for k in self.dataFrame.columns[1:]:
-      self.dataFrame[k], _ = pd.factorize(self.dataFrame[k])
+    """
+    Loads data file and prepares data
+    :param data_file: CSV file name
+    """
+    self.data_frame = pd.read_csv(data_file)
+    for k in self.data_frame.columns[1:]:
+      self.data_frame[k], _ = pd.factorize(self.data_frame[k])
 
-    self.classes = np.array(sorted(pd.Categorical(self.dataFrame['class']).categories))
-    self.features = self.dataFrame.columns[self.dataFrame.columns != 'class']
+    self.classes = np.array(sorted(pd.Categorical(self.data_frame['class']).categories))
+    self.features = self.data_frame.columns[self.data_frame.columns != 'class']
 
-  def __factorize(self, data):
+  @staticmethod
+  def __factorize(data):
     y, _ = pd.factorize(pd.Categorical(data['class']), sort=True)
     return y
 
+  def train(self, X, Y):
+    """
+    Train classifier. Should be implemented in subclass
+    :param X: training input samples
+    :param Y: target values
+    """
+    raise NotImplementedError
+
   def validation_data(self, folds):
-    df = self.dataFrame
+    """
+    Performs data splitting, classifier training and prediction for given #folds
+    :param folds: number of folds
+    :return: list of numpy.array pairs (prediction, expected)
+    """
+    df = self.data_frame
     response = []
 
     assert len(df) > folds
@@ -52,12 +78,27 @@ class MushroomProblem:
 
 
 class MushroomRegression(MushroomProblem):
+  """
+  Implementation if mushrooms classification problem with sklearn.DecisionTreeRegressor
+  """
+
   def train(self, X, Y):
-    reg = DecisionTreeRegressor()
-    reg = reg.fit(X, Y)
-    return reg
+    """
+    Train classifier.
+    :param X: training input samples
+    :param Y: target values
+    :return: regressor
+    """
+    regressor = DecisionTreeRegressor()
+    regressor = regressor.fit(X, Y)
+    return regressor
 
   def validate(self, folds):
+    """
+    Evaluate classifier using mean squared error
+    :param folds: number of folds
+    :return: list of MSE per fold
+    """
     responses = []
 
     for y_true, y_pred in self.validation_data(folds):
@@ -65,8 +106,18 @@ class MushroomRegression(MushroomProblem):
 
     return responses
 
+
 class MushroomClassifier(MushroomProblem):
+  """
+  Partial implementation of mushrooms classification problem
+  """
+
   def validate(self, folds):
+    """
+    Evaluate classifier using confusion matrices
+    :param folds: number of folds
+    :return: list of confusion matrices per fold
+    """
     confusion_matrices = []
 
     for test, training in self.validation_data(folds):
@@ -74,17 +125,40 @@ class MushroomClassifier(MushroomProblem):
 
     return confusion_matrices
 
-  def confusion_matrix(self, train, test):
+  @staticmethod
+  def confusion_matrix(train, test):
     return pd.crosstab(test, train, rownames=['actual'], colnames=['preds'])
 
+
 class MushroomForest(MushroomClassifier):
+  """
+  Implementation of mushrooms classification problem with sklearn.RandomForestClassifier
+  """
+
   def train(self, X, Y):
-    clf = RandomForestClassifier(n_jobs = 2)
-    clf = clf.fit(X, Y)
-    return clf
+    """
+    Train classifier.
+    :param X: training input samples
+    :param Y: target values
+    :return: classifier
+    """
+    classifier = RandomForestClassifier(n_jobs=2)
+    classifier = classifier.fit(X, Y)
+    return classifier
+
 
 class MushroomTree(MushroomClassifier):
+  """
+  Implementation of mushrooms classification problem with sklearn.DecisionTreeClassifier
+  """
+
   def train(self, X, Y):
-    clf = DecisionTreeClassifier()
-    clf = clf.fit(X, Y)
-    return clf
+    """
+    Train classifier.
+    :param X: training input samples
+    :param Y: target values
+    :return: classifier
+    """
+    classifier = DecisionTreeClassifier()
+    classifier = classifier.fit(X, Y)
+    return classifier
