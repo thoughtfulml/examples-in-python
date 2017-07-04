85b1d35c137665de9bd73ea90a0c26a32a73b7f3
Alexey Porotnikov <alexey.porotnikov@gmail.com>
2017-07-05 00:23:48 +0300

    consistent formatting and stylistic improvements

--- hidden_markov_model/corpus_parser.py
+++ hidden_markov_model/corpus_parser.py
@@ -1,44 +1,55 @@
+"""
+Chapter 6. Hidden Markov Model
+"""
 import collections
 from copy import copy
 
 
 class CorpusParser:
-    NULL_CHARACTER = 'START'
-    STOP = ' \n'
-    SPLITTER = '/'
+  """
+  This class is responsible for parsing the Brown Corpus.
+  """
+  NULL_CHARACTER = 'START'
+  STOP = ' \n'
+  SPLITTER = '/'
 
-    def __init__(self):
-        self.ngram = 2
+  def __init__(self):
+    self.ngram = 2
 
-    TagWord = collections.namedtuple('TagWord', ['word', 'tag'])
+  TagWord = collections.namedtuple('TagWord', ['word', 'tag'])
 
-    def parse(self, stream):
-        ngrams = self.ngram * [CorpusParser.TagWord(CorpusParser.NULL_CHARACTER,
-                                                    CorpusParser.NULL_CHARACTER)]
+  def parse(self, stream):
+    """
+    Parse Brown Corps
+    :param stream: string of text
+    :return: an iterator through ngraps
+    """
+    ngrams = self.ngram * [CorpusParser.TagWord(CorpusParser.NULL_CHARACTER,
+                                                CorpusParser.NULL_CHARACTER)]
+    word = ''
+    pos = ''
+    parse_word = True
+
+    for char in stream:
+      if char == '\t' or (len(word) == 0 and char in CorpusParser.STOP):
+        continue
+      elif char == CorpusParser.SPLITTER:
+        parse_word = False
+      elif char in CorpusParser.STOP:
+        ngrams.pop(0)
+        ngrams.append(CorpusParser.TagWord(word, pos))
+
+        yield copy(ngrams)
+
         word = ''
         pos = ''
         parse_word = True
+      elif parse_word:
+        word += char
+      else:
+        pos += char
 
-        for char in stream:
-            if char == '\t' or (len(word) == 0 and char in CorpusParser.STOP):
-                continue
-            elif char == CorpusParser.SPLITTER:
-                parse_word = False
-            elif char in CorpusParser.STOP:
-                ngrams.pop(0)
-                ngrams.append(CorpusParser.TagWord(word, pos))
-
-                yield copy(ngrams)
-
-                word = ''
-                pos = ''
-                parse_word = True
-            elif parse_word:
-                word += char
-            else:
-                pos += char
-
-        if len(word) > 0 and len(pos) > 0:
-            ngrams.pop(0)
-            ngrams.append(CorpusParser.TagWord(word, pos))
-            yield copy(ngrams)
+    if len(word) > 0 and len(pos) > 0:
+      ngrams.pop(0)
+      ngrams.append(CorpusParser.TagWord(word, pos))
+      yield copy(ngrams)
--- hidden_markov_model/tests/test_corpus_parser.py
+++ hidden_markov_model/tests/test_corpus_parser.py
@@ -4,31 +4,31 @@ from hidden_markov_model.corpus_parser import CorpusParser
 
 
 class TestCorpusParser(unittest.TestCase):
-    def setUp(self):
-        self.stream = '\tSeveral/ap defendants/nns ./.\n'
-        self.blank = '\t    \n'
+  def setUp(self):
+    self.stream = '\tSeveral/ap defendants/nns ./.\n'
+    self.blank = '\t    \n'
 
-    def test_parse(self):
-        """will parse a brown corpus line using the standard / notation"""
-        cp = CorpusParser()
+  def test_parse(self):
+    """will parse a brown corpus line using the standard / notation"""
+    cp = CorpusParser()
 
-        null = CorpusParser.TagWord('START', 'START')
-        several = CorpusParser.TagWord('Several', 'ap')
-        defendants = CorpusParser.TagWord('defendants', 'nns')
-        period = CorpusParser.TagWord('.', '.')
+    null = CorpusParser.TagWord('START', 'START')
+    several = CorpusParser.TagWord('Several', 'ap')
+    defendants = CorpusParser.TagWord('defendants', 'nns')
+    period = CorpusParser.TagWord('.', '.')
 
-        expectations = [
-            [null, several],
-            [several, defendants],
-            [defendants, period]
-        ]
+    expectations = [
+      [null, several],
+      [several, defendants],
+      [defendants, period]
+    ]
 
-        results = list(cp.parse(self.stream))
-        self.assertListEqual(expectations, results)
+    results = list(cp.parse(self.stream))
+    self.assertListEqual(expectations, results)
 
-    def test_blank(self):
-        """does not allow blank lines from happening"""
-        cp = CorpusParser()
+  def test_blank(self):
+    """does not allow blank lines from happening"""
+    cp = CorpusParser()
 
-        results = list(cp.parse(self.blank))
-        self.assertEqual(0, len(results))
+    results = list(cp.parse(self.blank))
+    self.assertEqual(0, len(results))
