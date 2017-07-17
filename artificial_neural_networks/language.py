39e16bde873de03c12e85f36793786987bf35de1
Alexey Porotnikov <alexey.porotnikov@gmail.com>
2017-07-18 01:13:01 +0300

    consistent formatting

--- artificial_neural_networks/language.py
+++ artificial_neural_networks/language.py
@@ -2,15 +2,15 @@ from .tokenizer import Tokenizer
 
 
 class Language(object):
-    def __init__(self, io, name):
-        self._name = name
-        self._vectors, self._characters = Tokenizer.tokenize(io)
+  def __init__(self, io, name):
+    self._name = name
+    self._vectors, self._characters = Tokenizer.tokenize(io)
 
-    @property
-    def name(self): return self._name
+  @property
+  def name(self): return self._name
 
-    @property
-    def vectors(self): return self._vectors
+  @property
+  def vectors(self): return self._vectors
 
-    @property
-    def characters(self): return self._characters
+  @property
+  def characters(self): return self._characters
--- artificial_neural_networks/tests/test_network.py
+++ artificial_neural_networks/tests/test_network.py
@@ -1,62 +1,61 @@
 # coding=utf-8
-from io import StringIO
-import codecs
 from glob import glob
-import os
-import re
 import unittest
 
+from io import StringIO
+import codecs
+import os
+import re
 from nose_parameterized import parameterized
-
 from artificial_neural_networks.language import Language
 from artificial_neural_networks.network import Network
 
 
 def language_name(file_name):
-    basename, ext = os.path.splitext(os.path.basename(file_name))
-    return basename.split('_')[0]
+  basename, ext = os.path.splitext(os.path.basename(file_name))
+  return basename.split('_')[0]
 
 
 def load_glob(pattern):
-    result = []
-    for file_name in glob(pattern):
-        result.append(Language(codecs.open(file_name, encoding='utf-8'),
-                               language_name(file_name)))
-    return result
+  result = []
+  for file_name in glob(pattern):
+    result.append(Language(codecs.open(file_name, encoding='utf-8'),
+                           language_name(file_name)))
+  return result
 
 
 class TestNetwork(unittest.TestCase):
-    matthew_languages = load_glob('data/*_0.txt')
-    acts_languages = load_glob('data/*_1.txt')
-    matthew_verses = Network(matthew_languages)
-    matthew_verses.train()
-    acts_verses = Network(acts_languages)
-    acts_verses.train()
+  matthew_languages = load_glob('data/*_0.txt')
+  acts_languages = load_glob('data/*_1.txt')
+  matthew_verses = Network(matthew_languages)
+  matthew_verses.train()
+  acts_verses = Network(acts_languages)
+  acts_verses.train()
 
-    @parameterized.expand('English Finnish German Norwegian Polish Swedish'.split())
-    def test_accuracy(self, lang):
-        """Trains and cross-validates with an error of 5%"""
-        print('Test for %s' % lang)
-        self.compare(self.matthew_verses, './data/%s_1.txt' % lang)
-        self.compare(self.acts_verses, './data/%s_0.txt' % lang)
+  @parameterized.expand('English Finnish German Norwegian Polish Swedish'.split())
+  def test_accuracy(self, lang):
+    """Trains and cross-validates with an error of 5%"""
+    print('Test for %s' % lang)
+    self.compare(self.matthew_verses, './data/%s_1.txt' % lang)
+    self.compare(self.acts_verses, './data/%s_0.txt' % lang)
 
-    def compare(self, network, file_name):
-        misses = 0.0
-        hits = 0.0
-        with codecs.open(file_name, encoding='utf-8') as f:
-            text = f.read()
+  def compare(self, network, file_name):
+    misses = 0.0
+    hits = 0.0
+    with codecs.open(file_name, encoding='utf-8') as f:
+      text = f.read()
 
-        for sentence in re.split(r'[\.!\?]', text):
-            language = network.predict(StringIO(sentence))
-            if language is None: continue
-            if language.name == language_name(file_name):
-                hits += 1
-            else:
-                misses += 1
+    for sentence in re.split(r'[\.!\?]', text):
+      language = network.predict(StringIO(sentence))
+      if language is None: continue
+      if language.name == language_name(file_name):
+        hits += 1
+      else:
+        misses += 1
 
-        total = misses + hits
+    total = misses + hits
 
-        self.assertLess(misses,
-                        0.05 * total,
-                        msg='%s has failed with a miss rate of %f' % (file_name,
-                                                                      misses / total))
+    self.assertLess(misses,
+                    0.05 * total,
+                    msg='%s has failed with a miss rate of %f' % (file_name,
+                                                                  misses / total))
